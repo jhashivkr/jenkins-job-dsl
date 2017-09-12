68c7effe1c25b568c7cfdd4d4dbe67b2e8cbf889
jhashivkr <jhashivkr@gmail.com>
2017-09-12 15:04:25 -0500

    committing changes

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,15 +1,15 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git@github.com:jhashivkr/jenkins-docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('jhashivkr')
+            node / gitConfigEmail('jhashivkr@gmail.com')
         }
     }
     triggers {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
+        nodejs('nodejs') // this is the name of the NodeJS installation in
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
